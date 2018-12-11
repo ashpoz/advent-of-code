a5a006333a3b4816e1ee59172778dedfc5d09c28
Adrian Haynes <adrianhaynes@Adrians-MacBook-Pro.local>
2018-12-10 23:45:43 -0600

    working on day 2

---
+++ Day 2/day2.js
@@ -0,0 +1,342 @@
+const ids = `qcsnyvpigkxmrdawlfdefotxbh
+qcsnyvligkymrdawljujfotxbh
+qmsnyvpigkzmrnawzjuefotxbh
+qosnyvpigkzmrnawljuefouxbh
+qcsnhlpigkzmrtawljuefotxbh
+qcsnyvpigkzmrdapljuyfotxih
+qcsnbvpiokzmrdawljuerotxbh
+qcfnyvmigkzmrdawljuefotdbh
+qcsnynpigkzmrdawljuefptxbp
+qcsgyapigkzmrdawljuafotxbh
+qcsnyvpigkzmrdapljueeotibh
+qcfnyvpigkzmndawljuwfotxbh
+qzsayvpigkzmrdawijuefotxbh
+qcsnsvpiekzmrdawljfefotxbh
+ncsnyvpigkzmrdaaljuefotxzh
+qssnyvpigkzmrdawljuefotobg
+qcshyipigkzmrdajljuefotxbh
+qcsnyvtigkzmrdawljgeaotxbh
+qcsnkvpxgkzmrdawljuefltxbh
+qcsnyvpiikzmrdawljuwfoqxbh
+qcsnybpigwzmqdawljuefotxbh
+qcsiyvpipkzbrdawljuefotxbh
+qldnyvpigkzmrdzwljuefotxbh
+qcsnyvpwgkzcrdawljuefmtxbh
+qcsnyvnigkzmrdahmjuefotxbh
+qcsnydpigkzmrdazljuefotxnh
+qcsqyvavgkzmrdawljuefotxbh
+ucsnyvpigkzmrdawljuefocxwh
+qcsnivpigrzmrdawljuefouxbh
+tcsnyvpibkzmrdawlkuefotxbh
+qcstytpigkzmrdawsjuefotxbh
+qcynyvpigkzmrdawlluefotjbh
+qcstyvpigkqrrdawljuefotxbh
+icsnyvpizkzmrcawljuefotxbh
+qcsnyvpimkzmrdavljuezotxbh
+qvsnoupigkzmrdawljuefotxbh
+qcsnyvpigkzmrdawwjuefftxgh
+qcpnyvpijkzmrdvwljuefotxbh
+qcsnyvpigkzmxdakdjuefotxbh
+jcsvyvpigkqmrdawljuefotxbh
+qcwnyvpigczmrsawljuefotxbh
+qcsnyvpdgkzmrdawljuefoixbm
+qysnyvpigkzmrdmwljuefotxbp
+qcsnavpigkzmrdaxajuefotxbh
+qcsfkvpigkzmrdawlcuefotxbh
+qcsnyvpigkvmrdawljcefotpbh
+qcsnyvpiqkkmrdawlvuefotxbh
+qhsnyvpigkzmrdawnjuedotxbh
+qasnlvpigkzmrdawljuefotxkh
+qgsnyvpigkzmrdabpjuefotxbh
+jcsnyvdigkzmrmawljuefotxbh
+qcsnivpigkzmrdawljuefonxth
+qcsnyjpigkzmrdawljgefotxmh
+qcstyvpigkzmrdacljuefovxbh
+qcsnvvpigkzmrdawljuewotrbh
+qcsnyvaigdzmrdawljueuotxbh
+qcsnyvpegkzmwdawljzefotxbh
+qcsnevpngkzmrdawlouefotxbh
+qcsnuvpigozmrdawljuefotdbh
+qgsnyvpigkzmqdayljuefotxbh
+qcsnyvpigkzmrdcwdjuofotxbh
+qcnnyvpigkzmrzawljuefstxbh
+qlsgyvpigkzmrdtwljuefotxbh
+qcsnyfpigkzlroawljuefotxbh
+qcsnkvwigkzmrdowljuefotxbh
+qcsnrvpigkzmrdawljuvfltxbh
+qcsnyvpigkzvreawljuefotxmh
+qcsrgvpigkzmrdawliuefotxbh
+qysnyvpigkzmrdawlxaefotxbh
+qcsnyvpigizmrdlwljuefotxbi
+qzsnyvpitkzmrdawljuefbtxbh
+qzgnyvpigkzmrdawljuefotxih
+qcsnyvpigkzmrdawlguefvtxbb
+qcsnyvpigkzmidawljuefouxjh
+qksnyvpigkzmrdawlruefotxhh
+qcsnyvpinkzmrdaaljuefotxah
+qcsnxvpigkzjrdawljuefhtxbh
+qcsnyvpigkzardawlgueuotxbh
+qcsnyvpiakzmrdpwljuefotxbt
+qcsnyvpigkzmrdawkjuefotxgb
+qcsnyvpigkzmrdawljuehocsbh
+qcsnsvpigktmrdawljuefotxvh
+qusnrvpigkzrrdawljuefotxbh
+qcsnyhiigkzmrdawrjuefotxbh
+qcsnavpigkzmrdawlfuefotxbz
+qcsnyvpigkzmmdamsjuefotxbh
+qcsnyvzigkzmrdcwljmefotxbh
+qcsnyvpigkzmriawljuefotbbe
+qcsnyvpigksmrdawljaefotxbd
+qcsnyvpigkzfrdawljuefoxxmh
+qcsnyvpygkrmrdawljuefotxbi
+qcsngvwigfzmrdawljuefotxbh
+qcsnyvpigkmkrdauljuefotxbh
+qcsnyvpigxzmrdgwljuefwtxbh
+qconyapigkzmrdaxljuefotxbh
+qcsnydpigkzwrdawljulfotxbh
+qcsnyvpimkzmmdawljuefotxch
+qcsnkspigkzmrdawgjuefotxbh
+qcsnyvpigkzmrdhwljfefbtxbh
+qcsnyipijkztrdawljuefotxbh
+qcseyvpigkrhrdawljuefotxbh
+qcsnyvpivkzmrdawljuefottbb
+qcsnyvpigkzmrdawlouefcjxbh
+qcsnyvpigkzmrgayljuefotxbm
+qcsnyvpvgkzmrdawrjujfotxbh
+qcsnyvpigkzmndawljuefqtxch
+qcsnyvpigbzmrdawljuefotibg
+qcsnyvpigkzmseawljuefotxbv
+qcsnwvpigkzmraawnjuefotxbh
+mcsnyvpiqkzmrdawljuefotlbh
+bcsnyvpigczmrdmwljuefotxbh
+qcsnyvpigkzmrtawljuegntxbh
+qcsnyvpijkzmrdawlmrefotxbh
+qdsnyvpfgkzmrdawljuekotxbh
+qcsnyvpigkzmrdawcjfegotxbh
+qcslyvphgkrmddawljuefotxbh
+qcsnyvpigkzmsdawkjuefojxbh
+qzsnyvpigkzmrzawljuefmtxbh
+qcsnyvpqgkzmcdawljuefttxbh
+qcsnyvpbgkpmrdawljuefoqxbh
+qcsnyvpigkemrdywljmefotxbh
+qcsnyypigkzmrdawljmefotxwh
+jcsnyvhwgkzmrdawljuefotxbh
+qcsnyvpigkzmrdawljurlotxwh
+qcsnnvpigzzmrdawljuefotwbh
+hcsnyvpigkzmrdarljuefitxbh
+qcsnyvpilkzmrfawljuefotsbh
+qcsnynpigkzmldawijuefotxbh
+qcsnyvpkgkjmrdawljuefotxlh
+qcsnylpigkzprdawljgefotxbh
+qcsnyvpigkzmrrawljnefohxbh
+qcsnivpigkzmrqawlbuefotxbh
+qcsgyvpigkzmrfawljuefotbbh
+qccuyvpigkzmrdawyjuefotxbh
+gcsnyvpigkzjrdawljuefotxby
+qcsmyvpiekzbrdawljuefotxbh
+qcsnyvpzgkrmrdawljuefotxbs
+qesnyvpigkzmpdqwljuefotxbh
+qcsnyvpigqzmrdawljuefutibh
+qcdnyvpigkzirdawljfefotxbh
+qcsnyvpiukzmrcrwljuefotxbh
+qcsnbvpickzmrdswljuefotxbh
+qcsnyvpighzmrpadljuefotxbh
+qccnyvpigkzmrdawljudxotxbh
+qcsnyvpigkzmrdabljuesotxlh
+qcsnyvpigkzmrrawlruefozxbh
+qconyzpigkzmrdawljuefotjbh
+qclnyvpigkzmrdxwljuefotbbh
+qcsnygpigkzmrdawlhuefooxbh
+qcsnyvpigkzmvdawljuefntxnh
+qcskyvpigkzmreawljuefotubh
+qrsnyvpxgkzmrdawljuefotxbz
+qclnyvpigtamrdawljuefotxbh
+qcsnyvpigkzmrdawojxefoyxbh
+qcsnyvpinkzmrdakljuwfotxbh
+qcsnyvpiykzmedawljuefgtxbh
+qcsayvpigkcmrdawijuefotxbh
+qcsnyvuiekzmrdamljuefotxbh
+qcdnyvpigkzmrdawnjuefoxxbh
+qcsnfvpwgszmrdawljuefotxbh
+qcsnycpigkzmrdawljqefotxih
+qcslyvphgkrmrdawljuefotxbh
+ecsnyvpigkzmrdawykuefotxbh
+qcsayvpigkzmraawljuekotxbh
+qcsnyvpigkdmrdawljuewofxbh
+qcznyvpigkzqrdawljuefotxnh
+qcsnyvplgkzmrdawljiefotlbh
+qcsnyvpigkzmroewljuefotbbh
+qcvnyvpigkzvrdawujuefotxbh
+qcanyypigkzmrdaeljuefotxbh
+qcsnyvwigkzmrdewljuefotxqh
+qcsryvpigkvmrdawljuefotabh
+pcsnyvpigkwmrdawljueforxbh
+qcsncvpigkzmrdawljuefotwmh
+qcsnyvpigozmrdawljudfozxbh
+qcsnynpigkzmrbawhjuefotxbh
+qcsnyvuigkzmrqawljuefotxch
+qcsnyvpickzmrdawljueeofxbh
+qcsnyvpigkzgrdawljueiouxbh
+qcsnyvpigkztrdawljuxnotxbh
+qcsnyvpigwzvrdawljfefotxbh
+qcsnyvpilkzmrdawljuefotxcz
+qcsnjvpigkzmrdawljuefoywbh
+qhsnyvpigyzmrdawljuhfotxbh
+qcsnyvpirkzmfdawljuffotxbh
+qcsjyvpigkzmvdawljuefotxzh
+qcszivpirkzmrdawljuefotxbh
+qwsnyvpigkzmtdawljuefetxbh
+qcrntvpigkzordawljuefotxbh
+qrsnyvpigkzmsdawljrefotxbh
+qcsnyviivkzmrdazljuefotxbh
+ecsnyvpigkzmrdawyjuefotxbw
+qnsnyvpkgkzmrdawljueqotxbh
+qcsyyppigkzmrdawljuefotxba
+qcsnyvpigkzhrdpwljuefouxbh
+ucsnyvpigkzmrdawojuefouxbh
+qysnyvpigkzmrdawljukfotxbd
+qcjnyvpigkzmrdalljfefotxbh
+fcsnyapigkmmrdawljuefotxbh
+qcnnkvpigkzmrdawljuefctxbh
+ocsnyvpigkzmsdawljuefotxbl
+qcsnyvpiakomrdawpjuefotxbh
+qcsnyvpigkzmrdawljvefbtxwh
+qcsnuvpigkzmvdfwljuefotxbh
+qcsnyapihkzmrdagljuefotxbh
+qzsnyvpigkzmrdawtjuefotxgh
+qcsnyvpigkzmrdawljuefomyah
+ocsnyvpigkzqrdawljuefotxbt
+qnsnyvpigkzmrdawljvevotxbh
+icsnyvpigkzmrdawljuefntxbt
+qcsnyvpigkzdrdawljuefotbbm
+scsnyvpigkzmrgawljuofotxbh
+qcsnydpigkzmrdowljuefotkbh
+qcsnyvtikkzmrdawljuefolxbh
+qcsiyvpigkcmrddwljuefotxbh
+qyrnyvpigkzmodawljuefotxbh
+pcsndvpfgkzmrdawljuefotxbh
+qcsnyvkigkhmriawljuefotxbh
+qcsnyvpigkzmsdmwlkuefotxbh
+dosnyvpigkzmrdawdjuefotxbh
+qcnnnvpigkzmrdzwljuefotxbh
+qcsnyvpivkumrdailjuefotxbh
+qcsnyvpigkzmrdswljuzfotxbz
+qcscynpigkzmrdawljuefotxbc
+qeanyvpigkzmrdawijuefotxbh
+qclnylpigkzmrdawljuefotxyh
+qcsnyvpigkzmrdawljbefowxbp
+qcsnyvpagkzmrdawljuefolebh
+qxsiyvpigkzmrdawljuefotxgh
+qcsnyvpigkynrdawljuefoqxbh
+qcsnevpigkzmrdxwgjuefotxbh
+qcsnyvpdgkzlrdawljeefotxbh
+qcsnyvpigkzmrgawljxbfotxbh
+ecsnyvpigkzmrdbwbjuefotxbh
+qcsnyvpigkzmraawujuefocxbh
+qcsnyvpihkzmrdawljuefouxbn
+fgsqyvpigkzmrdawljuefotxbh
+qcsnyvpigkmmrdawajuefotnbh
+qcsnyvvigkzmrdahljudfotxbh
+qcsnyvpixkzmrdqwljutfotxbh
+ncsnyvpickzmrdawljuehotxbh
+qcsnyvpizkzmrdawlpuefotxbp
+wcsnyvfigkzmrdakljuefotxbh
+qcsnyvpigkznrdhwljupfotxbh
+jcsnyvpigkpmzdawljuefotxbh
+qcsnyppigkkmrdawljujfotxbh
+qcsnyvpigkumrdaeljuefodxbh
+qcsnyvhigkzmrdrwljuefodxbh
+qcsnyvpigkacrdawtjuefotxbh
+qcsnyvpigkzmylawlquefotxbh`;
+
+const testIds = `abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab`;
+
+// testIds checksum = 12
+// 4 * 3 = 12
+
+const isTest = true;
+
+const parsedData = (isTest ? testIds : ids)
+  .split("\n");
+
+let twoMatch = 0;
+let threeMatch = 0;
+
+let sortedData = parsedData.slice().sort();
+let compareData = [];
+
+// console.log(data);
+
+for(let i = 0; i < sortedData.length; i++) {
+    let letters = sortedData[i].split("");
+    letters.sort();
+    console.log(letters);
+
+    let current = null;
+    let counter = 0;
+
+    for(let j = 0; j < letters.length; j++) {
+        if(letters[j] !== current) {
+            if(counter > 0) {
+            console.log(`${current} counted ${counter} times`);
+                if(counter == 2) {
+                    twoMatch += 1;
+                    compareData.push(counter);
+                } else if(counter == 3) {
+                    threeMatch += 1;
+                    compareData.push(counter);
+                } 
+            }
+            current = letters[j];
+            counter = 1;
+        } else {
+            counter++;
+        }
+    }
+    if (counter > 0) {
+        console.log(`${current} counted ${counter} timeS`);
+        if(counter == 2) {
+            twoMatch += 1;
+            compareData.push(counter);
+            console.log(compareData.slice(0, i + twoMatch));
+        } else if(counter == 3) {
+            threeMatch += 1;
+            compareData.push(counter);
+            console.log(compareData.slice(0, i));
+        } else {
+            console.log(compareData.slice(0, i));
+        }
+    }
+} 
+
+console.log(`Number of double matches are ${twoMatch}`);
+console.log(`Number of triple matches are ${threeMatch}`);
+console.log(compareData);
+
+let start = 0;
+let finish = 0;
+
+for(let i = 0; i < compareData.length; i++) {
+    // if endOfArray, slice previous values into new array
+    if(compareData[i] == "endOfArray") {
+        let newArr = compareData.slice(start, finish);
+        let index = newArr.indexOf("endOfArray");
+            if (index > -1) {
+                newArr.splice(index, finish);
+            }
+        console.log(newArr);
+        start = finish;
+    } else {
+        finish++;
+    }
+}
+
+
+
+
